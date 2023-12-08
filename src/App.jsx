4f0f5bf178671ea09c936c616c584422219aa0ea
Erfan-body-2003 <127528620+Erfan-body-2003@users.noreply.github.com>
2023-12-08 21:21:55 +0330

    setting up tailwindCss And delelope base route for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-
+import { Route, Routes } from "react-router-dom"
+import Index from './Pages'
+import Articles from './Pages/Articles'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Products from './Pages/Products'
+import SingleProduct from './Pages/SingleProduct'
+import SingleArticles from './Pages/SingleArticles'
 
 function App() {
 
 
   return (
     <>
+        <div>header</div>
+      <Routes>
+           <Route path="/" element={<Index/>}/>
+           <Route path="products" element={<Products/>}/>
+           <Route path="/articles" element={<Articles/>}/>
+           <Route path="/login" element={<Login/>}/>
+           <Route path="/register" element={<Register/>}/>
+           <Route path="/products/:productId" element={<SingleProduct/>}/>
+           <Route path="/articles/:articlesId" element={<SingleArticles/>}/>
+      </Routes>
+      <div>footer</div>
 
-    
-     
     </>
   )
 }
